60f1b808142675107637dae151e95be25ca85c5f
Nikhil Barasker <nikhilbaraskar20.nb@gmail.com>
2024-09-05 12:27:02 +0530

    claymagixclient

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ export default function App() {
         <Body onLogout={handleLogout} />
       ) : (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="w-[249px] ml-[430px]">
+          <div className="w-[249px] ml-[480px]">
             <img src={InfiniaLogo} alt="Infinia Logo" />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
